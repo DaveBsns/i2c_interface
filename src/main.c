e569b58ab2c4d7a0ca243814f10122aab2c5ace3
David Flaig <david-flaig@gmx.de>
2023-11-11 04:17:17 +0100

    finally its working now, values are the same than on arduinogit statusgit status

--- src/main.c
+++ src/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <util/delay.h>
 // #include <i2cmaster.h>
-// #include <lcd.h>
 #include <lib/hd44780pcf8574.h>
 #include <compat/twi.h>
 #include <stdbool.h>
+#include "library.c"
 
 
 #define F_CPU 4000000UL
@@ -31,19 +31,37 @@
 
 #define I2C_WRITE 0
 
+#define AC1_ADRESS 0xAA
+#define AC2_ADRESS 0xAC
+#define AC3_ADRESS 0xAE
+#define AC4_ADRESS 0xB0
+#define AC5_ADRESS 0xB2
+#define AC6_ADRESS 0xB4
+
+#define B1_ADRESS 0xB6
+#define B2_ADRESS 0xB8
+
+#define MB_ADRESS 0xBA
+#define MC_ADRESS 0xBC
+#define MD_ADRESS 0xBE
+
+
+// Temperature calibration values
 // use datasheet numbers!
-UP = 23843;
-ac6 = 23153;
-ac5 = 32757;
-mc = -8711;
-md = 2868;
-b1 = 6190;
-b2 = 4;
-ac3 = -14383;
-ac2 = -72;
-ac1 = 408;
-ac4 = 32741;
-oversampling = 0;
+
+short ac1;;
+short ac2;
+short ac3;
+unsigned short ac4;
+unsigned short ac5;
+unsigned short ac6;
+short b1;
+short b2;
+short mb;
+short mc;
+short md;
+
+long ut;
 
 lcd_initialized = false;
 
@@ -62,10 +80,10 @@ void display_text(char addr, int number){
     //HD44780_PCF8574_CursorBlink(addr);
 
     // Separate the first two digits and the last two digits
-    int first_two = number / 100;
+    int first_two = number / 10;
     int last_two = number % 100;
 
-    HD44780_PCF8574_DrawString(addr, "Temperatur:");
+    HD44780_PCF8574_DrawString(addr, "Temperature:");
     HD44780_PCF8574_PositionXY(addr, 0, 1);
     char number_str[16];
     sprintf(number_str, "%d.%02d C", first_two, last_two);
@@ -73,33 +91,11 @@ void display_text(char addr, int number){
     
 }
 
-
-void read_bmp180(char addr){
-    
-    //i2c_rep_start(addr+I2C_READ);
-    
-    
-    
-    //i2c_start_wait(BMP180_ADDRESS);
-    //str = i2c_read(0x2E);
-    //i2c_write(0xF4); // Control register for temperature reading
-    
-    // i2c_read(addr);
-    
-    //i2c_stop();
-
-}
-
 void i2c_initialize(void)
 {
-	
   /* initialize TWI clock: 100 kHz clock, TWPS = 0 => prescaler = 1 */
-  
   TWSR = 0;                         /* no prescaler */
   TWBR = ((F_CPU/SCL_CLOCK)-16)/2;  /* must be > 10 for stable operation */
-
-    
-
 }
 
 /*************************************************************************	
@@ -108,7 +104,6 @@ void i2c_initialize(void)
 *************************************************************************/
 unsigned char i2c_start(unsigned char address)
 {
-
     uint8_t   twst;
 
 	// send START condition
@@ -270,8 +265,6 @@ unsigned char i2c_rep_start(unsigned char address)
 *************************************************************************/
 unsigned char i2c_readNak(void)
 {
-    
-    
 	TWCR = (1<<TWINT) | (1<<TWEN);
     // blinki_blink();
 	while(!(TWCR & (1<<TWINT)));
@@ -287,8 +280,78 @@ int32_t computeB5(int32_t UT) {
   return X1 + X2;
 }
 
+uint16_t bmp180_get_cal_param(char addr){
+    unsigned char MSB;
+    unsigned char LSB;
+    uint16_t combinedValue;
+    
+    //Request Calibration value register
+    i2c_start_wait(BMP180_ADDRESS_WRITE);       // module adress 0x77 = 1110111 || + Write Bit (0) = 0xEE = 11101110 || + Acknowledge Bit (0) = 111011100
+    i2c_write(addr);                            // Register of the calibration value to read
+    i2c_stop();                                 // Stop Bit (1)
+
+    //Read calibration value split into MSB and LSB
+    i2c_start_wait(BMP180_ADDRESS_READ);        // module adress 0x77 = 1110111 || + Read Bit (1) = 0xEE = 11101111 || + Acknowledge Bit (0) = 111011100
+    MSB = i2c_readAck();                        // response || + Acknowledge Master (0)
+    LSB = i2c_readNak();                        // response || + Not Acknowledge Master (1)
+    i2c_stop();                                 // Stop Bit (1)
+
+    combinedValue = (uint16_t)MSB << 8 | LSB;
+
+    return combinedValue;
+}
+
+uint16_t bmp180_get_ut(){
+    unsigned char MSB;
+    unsigned char LSB;
+    uint16_t ut;
+
+    // Start Temperature reading
+    i2c_start_wait(BMP180_ADDRESS_WRITE);       // module adress 0x77 = 1110111 || + Write Bit (0) = 0xEE = 11101110 || + Acknowledge Bit (0) = 111011100
+    i2c_write(BMP180_CONTROL);                  // control register adress 0xF4 = 11110100 || + Acknowledge Bit (0) = 111101000              
+    i2c_write(BMP180_READTEMPCMD);              // Temperature Read Command Register 0x2E = 00101110 || + Acknowledge Bit (0) = 001011100       
+    i2c_stop();                                 // Stop Bit (1)
+    
+
+    //Request Temperature value
+    i2c_start_wait(BMP180_ADDRESS_WRITE);       // module adress 0x77 = 1110111 || + Write Bit (0) = 0xEE = 11101110 || + Acknowledge Bit (0) = 111011100
+    i2c_write(BMP180_TEMPDATA_ADRESS);          // Temperature Data Register 0xF6 = 11110110 || + Acknowledge Bit (0) = 111101100
+    i2c_stop();                                 // Stop Bit (1)
+    //Read Temperature split into MSB and LSB
+    i2c_start_wait(BMP180_ADDRESS_READ);        // module adress 0x77 = 1110111 || + Read Bit (1) = 0xEE = 11101111 || + Acknowledge Bit (0) = 111011100
+    MSB = i2c_readAck();                        // response || + Acknowledge Master (0)
+    LSB = i2c_readNak();                        // response || + Not Acknowledge Master (1)
+    i2c_stop();                                 // Stop Bit (1)
+    
+    // Combine MSB and LSB into a 16-bit value
+    ut = (uint16_t)MSB << 8 | LSB;
+
+    return ut;
+}
+
 
 int main(void) {
+    for(;;) {
+    i2c_initialize();  
+    ac1 = bmp180_get_cal_param(AC1_ADRESS);
+    ac2 = bmp180_get_cal_param(AC2_ADRESS);
+    ac3 = bmp180_get_cal_param(AC3_ADRESS);
+    ac4 = bmp180_get_cal_param(AC4_ADRESS);
+    ac5 = bmp180_get_cal_param(AC5_ADRESS);
+    ac6 = bmp180_get_cal_param(AC6_ADRESS);
+    b1 = bmp180_get_cal_param(B1_ADRESS);
+    b2 = bmp180_get_cal_param(B2_ADRESS);
+    mb = bmp180_get_cal_param(MB_ADRESS);
+    mc = bmp180_get_cal_param(MC_ADRESS);
+    md = bmp180_get_cal_param(MD_ADRESS);
+    ut = bmp180_get_ut();
+    int32_t tempVal = computeB5(ut);
+    int32_t temperature = (tempVal + 8) / (1 << 4);
+    display_text(LCD_ADDRESS, temperature);
+    _delay_ms(500);
+    }
+    
+    /*
     unsigned char MSB;
     unsigned char LSB;
     uint16_t combinedValue;
@@ -322,9 +385,14 @@ int main(void) {
         _delay_ms(200);
       
     }
+    */
 
     return 0;
 }
+
+
+
+
 
 
 
